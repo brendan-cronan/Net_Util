3a9f0094eb131a27f05a92311a6157268167fc4c
Brendan Cronan <32400374+brendan-cronan@users.noreply.github.com>
2017-10-26 16:56:39 -0400

    Added basic outline of functionality

--- src/Net_Util.java
+++ src/Net_Util.java
@@ -4,14 +4,17 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-
+/****************************************************
+*
+* This Class is intended to serve as a general utility class which should not
+* be used in a non-static way.
+*
+****************************************************/
 class Net_Util{
   public static final BufferedReader USER_INPUT=new BufferedReader(new InputStreamReader(System.in));
 
-  public Net_Util(){
 
-  }
-  public static Socket connectToServer(String serverIP,int portNumber) {
+  public static Socket connectToServer(String serverIP,int portNumber){
     try {
       return new Socket(serverIP,portNumber);
     } catch (UnknownHostException e) {
@@ -21,19 +24,89 @@ class Net_Util{
     }
     return null;
   }
+
+  //TODO: Edit this to actually list commands.
   public static String promptInput() {//return a string asking the user to enter commands and list the commands
-    return "";
+    String out="Please Enter a Command:\n> ";
+    return out;
   }
 
 
 
+  /**
+  * This is the start of the Send Functions.  This is an overloaded method
+  * that takes every type we may need and sends it to the server without worry.
+  **/
+  public static void send(Socket s, int i){
+    sendToServer("INT",i+"",s);
+  }
+  /*
+   *  This is adaptable to any type.  I just didnt feel like doing all types of arrays.
+   */
+  public static void send(Socket s, int[] i){
+    String out="";
+    for(int c:i)
+      out+=c+"|";
+    out=out.substring(0,out.length-1);
+    sendToServer("INTARR",out,s);
+  }
 
-  public void free() {
+  public static void send(Socket s, boolean b){
+    String bool=(b) ? "True":"False";
+    sendToServer("BOOL",bool,s);
+  }
+  public static void send(Socket s, Object o){
+    sendToServer("OBJ",o.toString(),s);
+  }
+  public static void send(Socket s, double d){
+    sendToServer("DOUBLE",d+"",s);
+  }
+  public static void send(Socket s, String str){
+    sendToServer("STRING",str,s);
+  }
+  public static void send(Socket s, String[] starr){
+    String out="";
+    for(String str:starr)
+      out+=str+"|";
+    out=out.substring(0,out.length-1);
+    sendToServer("STRINGARR",out,s);
+  }
+  /**
+  * This is the end of the Send block.
+  * TODO: expand upon this overloaded method to add functionality.
+  **/
+
+
+
+
+
+
+
+  private static void sendToServer(String type,String message,Socket s){
+    
+    switch(type){
+      case "INT":
+        break;
+      case "INTARR":
+        break;
+      case "BOOL":
+        break;
+      case "OBJ":
+        break;
+      case "DOUBLE":
+        break;
+      case "STRING":
+        break;
+      case "STRINGARR":
+        break;
+
+
+
+    }
 
   }
 
-public static void main(String[] args){
-  System.out.println("Hello!");
-}
+
+
 
 }
